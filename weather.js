84c6c6725df09429bca0faa8e1d10916cd102590
BrankoMili <105740275+BrankoMili@users.noreply.github.com>
2022-07-10 17:36:07 +0200

    Added find location with city and country parameters, added the Fahrenheit scale

--- weather.js
+++ weather.js
@@ -4,6 +4,7 @@ let temperature;
 let mintemp;
 let maxtemp;
 const getcurrentLocation = document.getElementById('location');
+let countryName = document.getElementById('namecountry');
 
 // Get weather info by input city/country name
 let submitInfo = (cityparameter, errorarg) => {
@@ -14,19 +15,38 @@ let submitInfo = (cityparameter, errorarg) => {
     let countryname = data.location.country;
     let citycountry = cityname + ', ' + countryname;
     document.getElementById('locationcitycountry').innerHTML = citycountry;
-    // console.log(data);
-    // current temperature
+    console.log(data);
+    // current temperature (fahrenheit and celsius options)
+    if (document.getElementById('scale').value === 'Celsius') {
     temperature = data.current.temp_c + '°C';
     document.getElementById('temperature').innerHTML = temperature;
     document.getElementById('wrong').innerHTML = '';
+    }
+    else {
+    temperature = data.current.temp_f + '°F';
+    document.getElementById('temperature').innerHTML = temperature;
+    document.getElementById('wrong').innerHTML = '';
+    }
 
-    // minimum temperature
+    // minimum temperature (fahrenheit and celsius options)
+    if (document.getElementById('scale').value === 'Celsius') {
     mintemp = data.forecast.forecastday[0].day.mintemp_c + '°C';
     document.getElementById('mintemp').innerHTML = mintemp;
+    }
+    else {
+        mintemp = data.forecast.forecastday[0].day.mintemp_f + '°F';
+    document.getElementById('mintemp').innerHTML = mintemp;
+    }
 
-    // maximum temperature
+    // maximum temperature (fahrenheit and celsius options)
+    if (document.getElementById('scale').value === 'Celsius') {
     maxtemp = data.forecast.forecastday[0].day.maxtemp_c + '°C';
     document.getElementById('maxtemp').innerHTML = maxtemp;
+    }
+    else {
+    maxtemp = data.forecast.forecastday[0].day.maxtemp_f + '°F';
+    document.getElementById('maxtemp').innerHTML = maxtemp;
+    }
 
     // weather description
     let description = data.current.condition.text;
@@ -51,11 +71,19 @@ let submitInfo = (cityparameter, errorarg) => {
     //displaying of forecast
     document.getElementById('forecast').style.display = 'block';
 
-    // minimum and maximum temperature tommorrow
+    // minimum and maximum temperature tommorrow (fahrenheit and celsius options)
+    if (document.getElementById('scale').value === 'Celsius') {
     let mintemptomorrow = data.forecast.forecastday[1].day.mintemp_c + '°C / ';
     document.getElementById('mintemptomorrow').innerHTML = mintemptomorrow;
     let maxtemptomorrow = data.forecast.forecastday[1].day.maxtemp_c + '°C';
     document.getElementById('maxtemptomorrow').innerHTML = maxtemptomorrow;
+    }
+    else {
+    let mintemptomorrow = data.forecast.forecastday[1].day.mintemp_f + '°F / ';
+    document.getElementById('mintemptomorrow').innerHTML = mintemptomorrow;
+    let maxtemptomorrow = data.forecast.forecastday[1].day.maxtemp_f + '°F';
+    document.getElementById('maxtemptomorrow').innerHTML = maxtemptomorrow;
+    }
 
         //weather icons tomorrow
     let iconnumbertomorrow = data.forecast.forecastday[1].day.condition.icon;
@@ -63,11 +91,19 @@ let submitInfo = (cityparameter, errorarg) => {
         let codetom = './weather/64x64/day/' + partoftom;
         document.getElementById('icontomorrow').src = codetom;
 
-    // minimum and maximum temperature for day after tomorrow
+    // minimum and maximum temperature for day after tomorrow (fahrenheit and celsius options)
+    if (document.getElementById('scale').value === 'Celsius') {
     let mintempdat = data.forecast.forecastday[2].day.mintemp_c + '°C / ';
     document.getElementById('mintempdat').innerHTML = mintempdat;
     let maxtempdat = data.forecast.forecastday[2].day.maxtemp_c + '°C';
     document.getElementById('maxtempdat').innerHTML = maxtempdat;
+    }
+    else {
+    let mintempdat = data.forecast.forecastday[2].day.mintemp_f + '°F / ';
+    document.getElementById('mintempdat').innerHTML = mintempdat;
+    let maxtempdat = data.forecast.forecastday[2].day.maxtemp_f + '°F';
+    document.getElementById('maxtempdat').innerHTML = maxtempdat;
+    }
 
     // Date of weather tomorrow and for day afer tomorrow
     let dateTomorrow = data.forecast.forecastday[1].date;
@@ -103,15 +139,27 @@ let submitInfo = (cityparameter, errorarg) => {
 
 // Submit on click/enter button
 submitButton.addEventListener("click", () => {
+    if (document.getElementById('advanced').checked === false) {
     let cityNamevalue = cityName.value;
-    submitInfo(cityNamevalue)
+    submitInfo(cityNamevalue);
+    }
+    else {
+    let citycountryName = cityName.value + ',' + countryName.value;
+    submitInfo(citycountryName);
+    };
 });
 
 window.addEventListener('keypress', function (e) {
-    let cityNamevalue = cityName.value;
     if (e.key === 'Enter') {
-        submitInfo(cityNamevalue);
+        if (document.getElementById('advanced').checked === false) {
+    let cityNamevalue = cityName.value;
+    submitInfo(cityNamevalue);
     }
+    else {
+    let citycountryName = cityName.value + ',' + countryName.value;
+    submitInfo(citycountryName);
+    }
+    };
 });
 
 // Get location functions
@@ -146,3 +194,19 @@ function getCoordintes() {
 // Button to get location
 getcurrentLocation.addEventListener("click", getCoordintes);
 
+
+// Advanced search button toggle
+let advancedSearch = document.getElementById('advanced');
+
+advancedSearch.addEventListener('click', () => {
+    if (document.getElementById('advanced').checked === true) {
+        document.getElementById('countryname').style.display = 'block';
+        document.getElementById('tempscale').style.display = 'block';
+        document.getElementsByTagName("input")[0].placeholder = 'City name';
+    }
+    else {
+        document.getElementById('countryname').style.display = 'none';
+        document.getElementById('tempscale').style.display = 'none';
+        document.getElementsByTagName("input")[0].placeholder = 'City/country';
+    }
+});
